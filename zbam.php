8c7b2de15f5dbba5dc58593f34722eb4da0652f0
mukrehan <mukrehan@gmail.com>
2026-06-23 00:53:00 +0530

    Initial commit of my profile

---
+++ zbam.php
@@ -0,0 +1,456 @@
+<html>
+<head><meta http-equiv='Content-Type' content='text/html; charset=gb2312'>
+<link rel='SHORTCUT ICON' href='http://i.hizliresim.com/MkqPaa.gif'>
+<meta name='description' content='nothingz' />
+<meta name='keywords' content='ringgo' />
+<meta name='author' content='
+_________________________________________
+|                                             |
+| Script Created By OmarBinHattap               |
+| FB   = https://www.facebook.com/sibersavascilar |
+| Nick = OmarBinHattap                          |
+| TURKEY                              |
+|_____________________________________________|' />
+<meta name='geo.country' content='id' />
+<meta name='geo.placename' content='Indonesia' />
+<meta name='robots' content='all'/>
+<meta name='robots' content='index, follow' />
+<meta name='robots schedule' content='auto'/>
+<meta name='googlebot' content='index,follow'/>
+<meta name='distribution' content='global'/> 
+<title>#HACKED BY ZBAM#</title>
+
+<script src='http://e-mete.com/js/kdsnow.js'></script>
+<style>
+
+.ringgo {
+  z-index: 1000;
+  height: 15px;
+  width: 280px;
+  border: 2px solid #ff0000;
+  background: #000000;
+  -moz-linear-gradient(top,#000,#D11717);
+  background: -webkit-gradient(linear, left top, left bottom, from(#000), to(#D11717));
+  border-radius: 12px;
+  -moz-border-radius:12px;
+  -webkit-border-radius: 12px;
+  -o-transition: all 1s ease-in-out;
+  -moz-transition: all 1s ease-in-out;
+  -webkit-transition: all 1s ease-in-out;
+  padding: 5px 5px;
+  margin:15px auto;
+  font: 8px Orbitron;
+  color: #FF0000;
+  overflow: hidden;
+  box-shadow: 0 1px 8px #000;
+  -moz-box-shadow: 0 1px 8px #000;
+  -webkit-box-shadow: 0 1px 8px #000;
+  text-shadow: 0 0 30px #00fffa;
+}
+.ringgo:hover {
+  min-height: 270px;
+  border: 2px solid #ff0000;
+  background: #000000;
+  box-shadow: 0 1px 15px #000;
+  -moz-box-shadow: 0 1px 15px #000;
+  -webkit-box-shadow: 0 1px 15px #000;
+  color: #000; 
+  text-shadow: 0 0 30px #00fffa;
+}
+.ringgo h3, .nothingz h3 {
+  font-size: 8px;
+  font-family: Orbitron;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+  text-shadow: 0 0 30px #00fffa;
+  margin: 3px 5px;
+  background: #000000;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  border: 1px solid #ff0000;
+  -o-transition: all 1s ease-in-out;
+  -moz-transition: all 1s ease-in-out;
+  -webkit-transition: all 1s ease-in-out;
+}
+.tejahide h3:hover{
+  box-shadow: 0 1px 8px #000;
+  -moz-box-shadow: 0 1px 8px #000;
+  -webkit-box-shadow: 0 1px 8px #000;
+  text-shadow: 0 0 30px #00fffa;
+}
+.ringgo img.mini, .ringgo img.minianima {
+  width: 70px;
+  border: 4px solid #ff0000;
+  padding: 3px;border-radius: 6px;
+  -moz-border-radius: 6px;
+  -webkit-border-radius: 6px;
+  float: left; margin: 0 10px 5px 0;background: #222;
+  -o-transition: all 1.5s;
+  -moz-transition: all 1.5s;
+  -webkit-transition: all 1.5s;
+  text-shadow: 0 0 30px #00fffa;
+}
+.ringgo img.mini:hover, .ringgo img.minianima:hover {
+  box-shadow: 1px 1px 15px #000;
+  -moz-box-shadow: 1px 1px 15px #000;
+  -webkit-box-shadow: 1px 1px 15px #000;
+  border: 4px solid #CCCCCC;
+  background :#000000;
+  -o-transform: scale(1.4);-moz-transform: scale(1.4);
+  -webkit-transform: scale(1.4);
+  margin-top: 20px;margin-left: 15px;
+  text-shadow: 0 0 30px #00fffa;
+}
+  .tejahide img.minianima:hover {
+  -o-transform: scale(1.4) rotate(360deg) translate(0px);
+  -moz-transform: scale(1.4) rotate(360deg) translate(0px);
+  -webkit-transform: scale(1.4) rotate(360deg) translate(0px);
+  text-shadow: 0 0 30px #00fffa;
+}
+.nothingz {
+  margin-top: 15px;
+  height:225px;
+  overflow: auto;
+  text-shadow: 0 0 30px #00fffa;
+  padding: 0 5px;
+  -o-transition: all 1s ease-in-out;
+  -moz-transition: all 1s ease-in-out;
+  -webkit-transition: all 1s ease-in-out;
+  background:#000000 -moz-linear-gradient(top,#000000,#000000);
+  background:-webkit-gradient(linear, left top, left bottom, from(#000000), to(#000000));
+  background-image:url('');
+}
+.nothingz:hover {
+  background: #000000;
+  color: #eee;
+  text-shadow: 0 0px 1px #fe0303;
+  text-shadow: 0 0 30px #00fffa;
+}
+.nothingz h3 {
+  margin: 20px 0;
+  max-width: 224px;
+  margin-left:10px;
+  background: #000000;
+  box-shadow: 0 1px 12px #eee;
+  -moz-box-shadow: 0 1px 12px #eee;
+  -webkit-box-shadow: 0 1px 12px #eee;
+  text-shadow: 0 0 30px #00fffa;
+}
+.columns{
+  clear: both;
+  line-height: 22px;
+  padding:0 0 20px;
+  width: 250px;
+  text-shadow: 0 0 30px #00fffa;
+}
+.colleft{
+  float:left;
+  line-height:22px;width:120px
+  text-shadow: 0 0 30px #00fffa;
+}
+.colright{
+  float:right;
+  line-height:22px;
+  width:120px
+  text-shadow: 0 0 30px #00fffa;
+}
+.h3:hover {
+  background: #000000;
+  border: 1px #ff0000;
+  solid #666;
+  box-shadow: 0 1px 12px #fff;
+  -moz-box-shadow: 0 1px 12px #fff;
+  -webkit-box-shadow: 0 1px 12px #fff;
+  text-shadow: 0 0 30px #00fffa;
+}
+.nothingz ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: #ffffff;
+  text-shadow: 0 0 30px #00fffa;
+}
+.nothingz li {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: white;
+  border-bottom:1px dotted #ff0000;
+  text-shadow: 0 0 30px #00fffa;
+  border:1px solid #ff0000;
+}
+.nothingz lz {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: white;
+  text-shadow: 0 0 30px #ff0000;
+  border:1px #ff0000;
+  font-size: 12px;
+  font-family: arial;
+  font-weight: bold;
+}
+.nothingz li a{
+  color: #FF0000;
+  padding: 0;
+  margin: 0;
+  text-decoration:none;font-size: 12px;
+  -o-transition: all 1.5s;
+  -moz-transition: all 1.5s;
+  -webkit-transition: all 1.5s;
+  text-shadow: 0 0 30px #00fffa;
+}
+.nothingz li a:hover {
+  -o-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  color: white;
+  text-shadow: 0 0 30px #00fffa;
+  margin-left: 20px;
+}
+.ringgo {
+  height: 17px;
+  float: right;
+  margin-top:0px;
+  z-index: 10000;
+  position: fixed;
+  top:0px;
+  margin-left:690px;
+  text-shadow: 0 0 30px #00fffa;
+} 
+.ringgo h3, .nothingz {
+  font-size: 11px;
+}</style>
+
+<style type='text/css'>
+      @import url('https://fonts.googleapis.com/css?family=Montserrat|Just+Another+Hand|Lobster|Niconne|Orbitron:200,300,400,600');
+
+.title {
+  z-index: 10;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  font-family: 'Montserrat';
+  text-align: center;
+  width: 100%;
+}
+.title h1 {
+  position: relative;
+  color: #EEEEEE;
+  font-weight: 600;
+  font-size: 60px; 
+  padding: 10;
+  margin: 0;
+  line-height: 1;
+  text-shadow: 0 0 30px #000155;
+  top: 25%;
+  bottom: 25%;
+  left: 0;
+  right: 0;
+  border: none;
+  border-bottom: 1px solid white;
+  width: 50%;
+  text-align: center;
+}
+.h1:bofore {
+  position: relative;
+  color: #EEEEEE;
+  font-weight: 500;
+  font-size: 50px;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  text-shadow: 0 0 30px #000155;
+  top: 25%;
+  bottom: 25%;
+  left: 0;
+  right: 0;
+}
+.title h1 span {
+  font-weight: 600;
+  padding: 0;
+  margin: 0;
+  color: #BBB;
+}
+.title h2 {
+  font-weight: 200;
+  font-size: 20px;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  color: #EEEEEE;
+  text-shadow: 0 0 30px #000155;
+}
+.title h4 {
+  background-color:#000000;
+  font-weight: 100;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  border:1px solid; color: red;
+  color: red;
+  text-shadow: 0 0 30px #00fffa;
+  font-family: 'Just Another Hand';
+}
+.title h3 {
+  font-weight: 200;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  color: #ffffff;
+  text-shadow: 0 0 30px #00fffa;
+  font-family: 'Just Another Hand';
+  font-size: 30px;
+}
+.title h5 {
+
+  font-weight: 200;
+  font-size: 15px;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+  text-shadow: 0 0 8px #ff0000;
+  font-family: 'Niconne';
+  font-size: 15px;
+}
+
+audio
+{
+-webkit-transition:all 0.5s linear;
+-moz-transition:all 0.5s linear;
+-o-transition:all 0.5s linear;
+transition:all 0.5s linear;
+-moz-box-shadow: 2px 2px 4px 0px #006773;
+-webkit-box-shadow:  2px 2px 4px 0px #006773;
+box-shadow: 2px 2px 4px 0px #006773;
+-moz-border-radius:7px 7px 7px 7px ;
+-webkit-border-radius:7px 7px 7px 7px ;
+border-radius:7px 7px 7px 7px ;
+}
+
+audio:hover, audio:focus, audio:active
+{
+-webkit-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-moz-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-webkit-transform: scale(1.05);
+-moz-transform: scale(1.05);
+transform: scale(1.05);
+}
+
+</style>
+
+<div class='ringgo'>
+<h3>
+<center>Biz Kimiz ??</h3>
+<div class='nothingz'>
+<h3>website</h3>
+<ul>
+<li><center><a href='#' target='_blank' title='Türk Siber Konseyi'>ZBAM</a></li></center>
+<ul>
+</br>
+<img src='http://i.hizliresim.com/MkqPaa.gif' width='240' height='180'></ul>
+<ul>
+<h3>Copyright By OmarBinHattap</h3>
+</ul>
+<center><audio preload='metadata' allow='autoplay' src='https://soundcloud.com/gencmuslumanlar/allahu-akbar-hattem-arabic-nasheed' controls='autoplay' __idm_id__='205838337'preload' preload preload='metadata' autoplay controls loop></audio>
+<ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</head>
+<body translate='no' oncontextmenu='return true;' onkeydown='return true;' onmousedown='return true;'>
+  <div class='title' align='center'>
+  <div id='after'>
+
+           
+
+<center><b><marquee scrollamount='100' direction='right' size='50'><font size='6' color='white' style='text-shadow: 0 0 30px #00fffa;'>--------------------------------------------------------------------------------</marquee></font></h3></center></b>
+
+           <center><h5><b><font face='Orbitron' color='white' size='50'>Hacked By ZBAM</font></h5></center>
+
+<center><b><marquee scrollamount='100' direction='life' size='50'><font size='6' color='white' style='text-shadow: 0 0 30px #00fffa;'>--------------------------------------------------------------------------------</marquee></font></h3></center></b>
+
+           </br><span class='h3'><h3><font face='Just Another Hand' style='background-color:#000000;'>ALLAHU EKBER</br></h3></p>
+    
+           <h5><font size='6' color='white'>HACKED BY ZBAM</h5>
+    
+           <h4><font color='white' size='6'>- Türk Siber Konseyi -</font></center></marquee>
+</div>
+
+
+<style type='text/css'>
+
+
+body{
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-position: center;
+	background-color:#000000;
+  height:100%;
+  width:100%;
+  background-attachment: fixed;
+  text-shadow: 0 0 8px #ff0000;
+  background-size:100% 100%;
+	background-image:url('https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+  cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur862.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur862.png), progress !important;
+}
+
+
+ul,li{
+text-indent:0;text-decoration:none;margin:0;padding:0
+}
+
+audio
+{
+-webkit-transition:all 0.5s linear;
+-moz-transition:all 0.5s linear;
+-o-transition:all 0.5s linear;
+transition:all 0.5s linear;
+-moz-box-shadow: 2px 2px 4px 0px #006773;
+-webkit-box-shadow:  2px 2px 4px 0px #006773;
+box-shadow: 2px 2px 4px 0px #006773;
+-moz-border-radius:7px 7px 7px 7px ;
+-webkit-border-radius:7px 7px 7px 7px ;
+border-radius:7px 7px 7px 7px ;
+}
+
+audio:hover, audio:focus, audio:active
+{
+-webkit-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-moz-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-webkit-transform: scale(1.05);
+-moz-transform: scale(1.05);
+transform: scale(1.05);
+}
+
+img{
+border:0
+}
+
+canvas{
+cursor:crosshair;
+display:block;
+left:0;
+position:absolute;
+top:0;
+z-index:20;
+
+}
+
+</style>
+
+<marquee direction='life' scrolldelay='250' style='position:fixed; bottom:0px; left:0; width:100%;'><img class="fg" src='http://i.hizliresim.com/MkqPaa.gif' style='border:0; background:none; padding:0; box-shadow: none; width:80px;'/></marquee>
+
+<script src='http://parsico.net/pages/fw/s.js'></script>
+<script src='http://parsico.net/pages/fw/f.js'></script>
+
+</script></body></html>
